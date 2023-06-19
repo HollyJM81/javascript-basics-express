04981a3eb9a20550a14dbf4e3e19e1300e117407
Holly Mathieson <hjmathieson@gmail.com>
2023-06-19 10:48:51 +0100

    refactored app.js to take sayHello function, rather than hard-coded

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const { sayHello } = require('./lib/strings');
 
 const app = express();
 
+
 app.get('/strings/hello/:string', (req, res) => {
-  res.status(200).json({ result: `Hello, ${req.params.string}!` });
+  res.json({ result: sayHello(req.params.string) });
 });
+
+app.get('/upper/:string', (req, res) => {
+  res.json({result: })
+})
 
 module.exports = app;
