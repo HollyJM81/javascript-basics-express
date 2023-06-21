71ea239994714fb70193d95b9e46ea7f762ba293
Holly Mathieson <hjmathieson@gmail.com>
2023-06-21 21:48:57 +0100

    string tests passing

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 
-const { sayHello } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters } = require('./lib/strings');
+
+
+
+
 
 const app = express();
 
 
 app.get('/strings/hello/:string', (req, res) => {
-  res.json({ result: sayHello(req.params.string) });
+  res.status(200).json({ result: sayHello(req.params.string) });
 });
 
-app.get('/upper/:string', (req, res) => {
-  res.json({result: })
-})
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string)})
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (!req.query.length) {
+    res.status(200).send({ result: firstCharacter(req.params.string) });
+  }
+  res.status(200).send({ result: firstCharacters(req.params.string, req.query.length) });
+});
 
 module.exports = app;
